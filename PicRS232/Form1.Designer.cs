f08266494cdfc6b4859e59ddef3c7edcf11b7010
ilies bourouh <ilies.bourouh@univ-bba.dz>
2019-09-12 18:20:42 +0100

    first push

---
+++ PicRS232/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PicRS232
+{
+    partial class Form1_Principal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_0 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label_mensaje_pic = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_3
+            // 
+            this.button_3.Location = new System.Drawing.Point(232, 41);
+            this.button_3.Margin = new System.Windows.Forms.Padding(4);
+            this.button_3.Name = "button_3";
+            this.button_3.Size = new System.Drawing.Size(86, 28);
+            this.button_3.TabIndex = 0;
+            this.button_3.Text = "Total";
+            this.button_3.UseVisualStyleBackColor = true;
+            this.button_3.Click += new System.EventHandler(this.button_total_Click);
+            // 
+            // button_1
+            // 
+            this.button_1.Location = new System.Drawing.Point(138, 41);
+            this.button_1.Margin = new System.Windows.Forms.Padding(4);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(86, 28);
+            this.button_1.TabIndex = 1;
+            this.button_1.Text = "1";
+            this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button_1_Click);
+            // 
+            // button_0
+            // 
+            this.button_0.Location = new System.Drawing.Point(36, 41);
+            this.button_0.Margin = new System.Windows.Forms.Padding(4);
+            this.button_0.Name = "button_0";
+            this.button_0.Size = new System.Drawing.Size(94, 28);
+            this.button_0.TabIndex = 2;
+            this.button_0.Text = "0";
+            this.button_0.UseVisualStyleBackColor = true;
+            this.button_0.Click += new System.EventHandler(this.button_0_Click);
+            // 
+            // button_2
+            // 
+            this.button_2.Location = new System.Drawing.Point(326, 41);
+            this.button_2.Margin = new System.Windows.Forms.Padding(4);
+            this.button_2.Name = "button_2";
+            this.button_2.Size = new System.Drawing.Size(86, 28);
+            this.button_2.TabIndex = 3;
+            this.button_2.Text = "3";
+            this.button_2.UseVisualStyleBackColor = true;
+            this.button_2.Click += new System.EventHandler(this.button_3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(428, 25);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(69, 20);
+            this.toolStripStatusLabel1.Text = "hh:mm:ss";
+            // 
+            // label_mensaje_pic
+            // 
+            this.label_mensaje_pic.AutoSize = true;
+            this.label_mensaje_pic.Location = new System.Drawing.Point(33, 180);
+            this.label_mensaje_pic.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_mensaje_pic.Name = "label_mensaje_pic";
+            this.label_mensaje_pic.Size = new System.Drawing.Size(150, 17);
+            this.label_mensaje_pic.TabIndex = 5;
+            this.label_mensaje_pic.Text = "Message from the PIC:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 2400;
+            this.serialPort1.PortName = "COM2";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(36, 93);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(43, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(224, 180);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "enter";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 327);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_mensaje_pic);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_0);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button_3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PicRS232";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_0;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label_mensaje_pic;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PicRS232/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO.Ports; // Do not forget.
+
+namespace PicRS232
+{
+    public partial class Form1_Principal : Form
+    {
+        // Use a string as the receive buffer.
+        string Recibidos;
+        public Form1_Principal()
+        {
+            InitializeComponent();
+            // Open port while running the application.
+            if (!serialPort1.IsOpen)
+            {
+                try
+                {
+                    //serialPort1.Encoding = Encoding.UTF32;
+                    //serialPort1.Open();
+                    
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            // Run the Reception function by triggering the Event 'DataReived'.
+            serialPort1.DataReceived += new
+            System.IO.Ports.SerialDataReceivedEventHandler(Recepcion);
+        }
+        
+        // Process the data received in the full frame buffer and extract.
+        private void Actualizar(object s, EventArgs e)
+        {
+            // Assign the value of the plot to the RichTextBox.
+            //richTextBox_visualizar_mensaje.Text = Recibidos;
+        }
+        private void button_total_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(new Byte[] { 24, 115, 0 }, 0, 3);
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(new char[] { '←', 's', '2' }, 0, 3);
+        }
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(new char[] { '←', 's', '1' }, 0, 3);
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            //ReadFromSerial();
+        }
+        
+        // Show local time.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            statusStrip1.Items[0].Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(new char[] {'↑'}, 0, 1);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string str = Convert.ToString((char)27) + 'Q' + 'A' + textBox1.Text;
+            
+            serialPort1.Write(str);
+
+        }
+        private void ReadFromSerial()
+        {
+            try
+            {
+                SerialPort Serial1 = new System.IO.Ports.SerialPort("COM2", 2400, Parity.None, 8, StopBits.One);
+                Serial1.DtrEnable = true;
+                Serial1.RtsEnable = true;
+                Serial1.ReadTimeout = 3000;
+
+                var MessageBufferRequest = new byte[8] { 27 , 115 , 4 ,0,0,0,0,0 };
+                var MessageBufferReply = new byte[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+                int BufferLength = 8;
+
+                if (!Serial1.IsOpen)
+                {
+                    Serial1.Open();
+                }
+
+                try
+                {
+                    Serial1.Write(MessageBufferRequest, 0, BufferLength);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                System.Threading.Thread.Sleep(100);
+
+                Serial1.Read(MessageBufferReply, 0, 7);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SerialPort sp = new SerialPort();
+
+            sp.PortName = "COM2";
+            sp.BaudRate = 2400;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            //sp.Encoding = Encoding.UTF8;
+            string str = Convert.ToString((char)27) + 'Q' + 'A' + textBox2.Text+'♪';
+            sp.Open();
+            sp.Write(textBox2.Text);
+
+            sp.Close();
+            sp.Dispose();
+            sp = null;
+        }
+
+        // We always show the message below.
+        /*
+        private void richTextBox_visualizar_mensaje_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox_visualizar_mensaje.SelectionStart = richTextBox_visualizar_mensaje.TextLength;
+            richTextBox_visualizar_mensaje.ScrollToCaret();
+        }
+        */
+    }
+}
